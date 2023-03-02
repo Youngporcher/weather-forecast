adb32ac89fcfb26ce1741f855fed289357240c42
Alexey <87391704+Youngporcher@users.noreply.github.com>
2023-03-02 21:48:26 +0300

    upd

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,46 @@ const sunriseOut = document.getElementById('sunrise')
 const sunsetOut = document.getElementById('sunset')
 const maxTempOut = document.getElementById('maxTemp')
 const minTempOut = document.getElementById('minTemp')
+const weatherInfoDailyOut = document.getElementById('weatherInfoDaily')
 
 const precipitationOut = document.getElementById('precipitation')
 const humidityOut = document.getElementById('humidity')
 const tempOut = document.getElementById('temp')
+const pressureOut = document.getElementById('pressure')
+const windSpeedOut = document.getElementById('windSpeed')
+
+const weatherInfoHourlyOut = document.getElementById('weatherInfoHourly')
+
+const wmoCodes = {
+    0:'Ясное небо',
+    1:'Преимущественно ясно',
+    2:'Переменная облачность',
+    3:'Пасмурно',
+    45:'Туман',
+    48:'Осаждающий инейный туман',
+    51:'Легкая морось',
+    53:'Умеренная морось',
+    55:'Плотная морось',
+    56:'Легкая ледяная морось',
+    57:'Плотная ледяная морось',
+    61:'Слабый дождь',
+    63:'Умеренный дождь',
+    65:'Сильный дождь',
+    66:'Легкий ледяной дождь',
+    67:'Сильный ледяной дождь',
+    71:'Легкий снегопад',
+    73:'Умеренный снегопад',
+    75:'Сильный снегопад',
+    77:'Снежная крупа',
+    80:'Слабый ливень',
+    81:'Умеренный ливень',
+    82:'Сильный ливень',
+    85:'Слабый снег',
+    86:'Сильный снег',
+    95:'Умеренная гроза',
+    96:'Гроза со слабым градом',
+    99:'Гроза с сильным градом',
+}
 // Получить время (YYYY-MM-DD)
 function getDateNow(date){
     let month = date.getMonth()+1 < 10 ? '0' + (date.getMonth()+1) : date.getMonth()+1
@@ -29,7 +65,7 @@ function init(){
     });
     // Запрос к метео Api и Прорисовка данных на сайте
     function getMeteoData(){
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coords[0]}&longitude=${coords[1]}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,windspeed_10m,winddirection_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&windspeed_unit=ms&timezone=auto&start_date=${getDateNow(date)}&end_date=${getDateNow(date)}`)
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coords[0]}&longitude=${coords[1]}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,pressure_msl,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&windspeed_unit=ms&timezone=auto&start_date=${getDateNow(date)}&end_date=${getDateNow(date)}`)
             .then(response => response.json())
             .then(data => {
                 // Суточные показатели
@@ -37,13 +73,18 @@ function init(){
                 sunsetOut.innerHTML = data.daily.sunset[0].slice(-5)
                 maxTempOut.innerHTML = data.daily.temperature_2m_max[0] + '°C'
                 minTempOut.innerHTML = data.daily.temperature_2m_min[0] + '°C'
+                weatherInfoDailyOut.innerHTML = wmoCodes[data.daily.weathercode[0]]
 
-                precipitationOut.innerHTML = data.hourly.precipitation[date.getHours()] + ' mm'
+                // Часовые показатели
+                precipitationOut.innerHTML = data.hourly.precipitation[date.getHours()] + ' мм'
                 humidityOut.innerHTML = data.hourly.relativehumidity_2m[date.getHours()] + '%'
                 tempOut.innerHTML = data.hourly.temperature_2m[date.getHours()] + '°C'
+                pressureOut.innerHTML = Math.round(data.hourly.pressure_msl[date.getHours()]/1.333) + ' мм'
+                windSpeedOut.innerHTML = Math.round(data.hourly.windspeed_10m[date.getHours()]) + ' м/с'
+                weatherInfoHourlyOut.innerHTML = wmoCodes[data.hourly.weathercode[0]]
+
             })
     }
-
     // Клик по карте
     myMap.events.add('click', function (e) {
 
